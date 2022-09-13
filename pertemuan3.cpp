8ebf9f24b190d5021e2c0410799754acdde71293
M.AlifqiAnnurZaqiA11202214658 <112920497+alifqi@users.noreply.github.com>
2022-09-13 11:20:20 +0700

    Add files via upload

---
+++ pertemuan3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Muhammad Alifqi Annur Zaqi\nA11.2022.14658\nSMK N 3 KENDAL" << endl;
+
+    int Umur=18;
+    float tinggi=1.7;
+    char k='L';
+    string l="laki-laki";
+
+    cout << "\nUmur :" <<Umur;
+    cout << "\nTingggi :" <<tinggi;
+    cout << "\nKelamin :" <<k;
+    cout << "\nKelamin (string) :" <<l;
+    return 0;
+}
